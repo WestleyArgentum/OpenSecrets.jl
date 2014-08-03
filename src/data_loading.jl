3eec91395f30fb6f2a8ade3b6f591fb716c20ddd
WestleyArgentum <WestleyArgentum@gmail.com>
2014-08-03 14:16:02 -0400

    better formatting of data

--- src/data_loading.jl
+++ src/data_loading.jl
@@ -1,11 +1,18 @@
 
-function format_pacs_to_candidates_pre2012(raw_data)
-    DataFrame(Cycle = raw_data[:, 1], FECRecNo = raw_data[:, 2], PACID = raw_data[:, 3], CID = raw_data[:, 4],
-        Amount = raw_data[:, 5], Date = raw_data[:, 6], RealCode = raw_data[:, 7], Type = raw_data[:, 8],
-        DI = raw_data[:, 9], FECCandID = raw_data[:, 10])
+function strip_brackets(str)
+    str[2:end-1]
 end
 
-function format_pacs_to_candidates_post2012(raw_data)
+function format_pacs_to_candidates(raw_data)
+    raw_data[:, 1] = map!(c->int(strip_brackets(c)), raw_data[:, 1])
+    raw_data[:, 2] = map!(c->strip_brackets(c), raw_data[:, 2])
+    raw_data[:, 3] = map!(c->strip_brackets(c), raw_data[:, 3])
+    raw_data[:, 4] = map!(c->strip_brackets(c), raw_data[:, 4])
+    raw_data[:, 7] = map!(c->strip_brackets(c), raw_data[:, 7])
+    raw_data[:, 8] = map!(c->strip_brackets(c), raw_data[:, 8])
+    raw_data[:, 9] = map!(c->strip_brackets(c), raw_data[:, 9])
+    raw_data[:, 10] = map!(c->strip_brackets(c), raw_data[:, 10])
+
     DataFrame(Cycle = raw_data[:, 1], FECRecNo = raw_data[:, 2], PACID = raw_data[:, 3], CID = raw_data[:, 4],
         Amount = raw_data[:, 5], Date = raw_data[:, 6], RealCode = raw_data[:, 7], Type = raw_data[:, 8],
         DI = raw_data[:, 9], FECCandID = raw_data[:, 10])
@@ -19,5 +26,5 @@ function load_pacs_to_candidates(year)
     data_path = joinpath(_campaign_finance_data_sources[short_year], "pacs$(short_year_str).txt")
     raw_data = readcsv(data_path)
 
-    int(year) >= 2012 ? format_pacs_to_candidates_post2012(raw_data) : format_pacs_to_candidates_pre2012(raw_data)
+    format_pacs_to_candidates(raw_data)
 end
