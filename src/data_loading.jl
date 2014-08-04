0844a3fb6de58e0d75c5aa88ae20b4350486a884
WestleyArgentum <WestleyArgentum@gmail.com>
2014-08-04 11:22:53 -0400

    make loading more generic

--- src/data_loading.jl
+++ src/data_loading.jl
@@ -3,7 +3,20 @@ function strip_brackets(str)
     str[2:end-1]
 end
 
-function format_pacs_to_candidates(raw_data)
+function short_year(year)
+    short_year_str = string(year)[end-1:end]
+    (short_year_str, int(short_year_str))
+end
+
+function load_campaign_finance_data(data_str, year)
+    short_year_str, short_year_int = short_year(year)
+
+    data_path = joinpath(_campaign_finance_data_sources[short_year_int], "$(data_str)$(short_year_str).txt")
+    readcsv(data_path)
+end
+
+function load_pacs_to_candidates(year)
+    raw_data = load_campaign_finance_data("pacs", year)
     raw_data[:, 1] = map!(c->int(strip_brackets(c)), raw_data[:, 1])
     raw_data[:, 2] = map!(c->strip_brackets(c), raw_data[:, 2])
     raw_data[:, 3] = map!(c->strip_brackets(c), raw_data[:, 3])
@@ -16,15 +29,4 @@ function format_pacs_to_candidates(raw_data)
     DataFrame(Cycle = raw_data[:, 1], FECRecNo = raw_data[:, 2], PACID = raw_data[:, 3], CID = raw_data[:, 4],
         Amount = raw_data[:, 5], Date = raw_data[:, 6], RealCode = raw_data[:, 7], Type = raw_data[:, 8],
         DI = raw_data[:, 9], FECCandID = raw_data[:, 10])
-end
-
-
-function load_pacs_to_candidates(year)
-    short_year_str = string(year)[end-1:end]
-    short_year = int(short_year_str)
-
-    data_path = joinpath(_campaign_finance_data_sources[short_year], "pacs$(short_year_str).txt")
-    raw_data = readcsv(data_path)
-
-    format_pacs_to_candidates(raw_data)
 end
