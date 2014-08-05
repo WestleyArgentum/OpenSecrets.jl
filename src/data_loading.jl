d94aaf3af4dd5bbbd541b5292119a1ddb6bef880
WestleyArgentum <WestleyArgentum@gmail.com>
2014-08-05 11:36:34 -0400

    more obvious errors when datasource not found

--- src/data_loading.jl
+++ src/data_loading.jl
@@ -4,9 +4,11 @@ function short_year(year)
     (short_year_str, int(short_year_str))
 end
 
+# -------
+
 function load_pacs_to_candidates(year)
     short_year_str, short_year_int = short_year(year)
-    data_path = joinpath(_campaign_finance_data_sources[short_year_int], "pacs$(short_year_str).txt")
+    data_path = joinpath(get(_campaign_finance_data_sources, short_year_int, ""), "pacs$(short_year_str).txt")
 
     readtable(data_path, separator = ',', header = false, quotemark = ['|'],
               names = [:Cycle, :FECRecNo, :PACID, :CID, :Amount, :Date, :RealCode, :Type, :DI, :FECCandID],
@@ -14,9 +16,11 @@ function load_pacs_to_candidates(year)
               UTF8String, UTF8String])
 end
 
+# -------
+
 function load_committee_table(year)
     short_year_str, short_year_int = short_year(year)
-    data_path = joinpath(_campaign_finance_data_sources[short_year_int], "cmtes$(short_year_str).txt")
+    data_path = joinpath(get(_campaign_finance_data_sources, short_year_int, ""), "cmtes$(short_year_str).txt")
 
     readtable(data_path; separator = ',', header = false, quotemark = ['|'], truestrings = ["1"], falsestrings = ["0"],
               names = [:Cycle, :CmteID, :PACShort, :Affiliate, :Ultorg, :RecipID, :RecipCode,
@@ -24,3 +28,6 @@ function load_committee_table(year)
               eltypes = [Int, UTF8String, UTF8String, UTF8String, UTF8String, UTF8String,
               UTF8String, UTF8String, UTF8String, UTF8String, UTF8String, UTF8String, Int, Bool])
 end
+
+# -------
+
