84e2fd14d4b578aed0ea3d3393346ee4d470d9da
WestleyArgentum <WestleyArgentum@gmail.com>
2014-08-06 15:01:48 -0400

    just replace backslashes with spaces, for now

--- src/opensecretsbuffer.jl
+++ src/opensecretsbuffer.jl
@@ -10,19 +10,8 @@ type OpenSecretsBuffer <: IO
 end
  
 function Base.read(os::OpenSecretsBuffer, ::Type{Uint8})
-    if os.add_backslash
-        os.add_backslash = false
-        return '\\'
-    end
-
-    curr = os.peek == nothing ? read(os.io, Uint8) : os.peek
-    peek = !eof(os.io) ? read(os.io, Uint8) : nothing
-
-    if curr == '\\' && peek == '|'
-        os.add_backslash = true
-    end
-
-    curr
+    c = Base.read(os.io, Uint8)
+    c == '\\' ? ' ' : c
 end
 
 Base.eof(os::OpenSecretsBuffer) = os.peek == nothing && eof(os.io)
